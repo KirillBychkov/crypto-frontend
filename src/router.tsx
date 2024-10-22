de88f43cf96f5d7abc0a263301ce7a306e4203c5
KirillBychkov <bychkovkirill@live.com>
2024-10-22 09:41:48 +0300

    init

--- src/router.tsx
+++ src/router.tsx
@@ -1,11 +1,11 @@
 import App from './App';
 import { createBrowserRouter, Navigate } from 'react-router-dom';
-import { Dairy } from './pages/dairy/components/Dairy';
-import { Deposit } from './pages/deposit/Deposit';
-import { Wrapper } from './common/Wrapper';
+import { Dairy } from '@/pages/dairy/components/Dairy';
+import { Deposit } from '@/pages/deposit/Deposit';
+import { Wrapper } from '@/common/Wrapper';
 // import { ChartPage } from "@/pages/chart/chart.tsx";
 // import { Arbitrage } from "@/pages/arbitrage/arbitrage.tsx";
-import { SharePage } from "@/pages/share/group.tsx";
+import { SharePage } from "@/pages/share/group";
 
 export const router = createBrowserRouter([
   {
